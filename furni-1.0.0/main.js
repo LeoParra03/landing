f98630ceb70640dc3b1d0a1a396c59ad6f28152a
LeoParra03 <lparraf2003@gmail.com>
2024-06-09 21:05:25 -0500

    submit

--- furni-1.0.0/main.js
+++ furni-1.0.0/main.js
@@ -7,3 +7,24 @@ let loaded = ( eventLoaded ) => {
   }
   window.addEventListener('load', loaded);
 
+const loaded1 = (eventLoaded) => {
+    let myform = document.getElementById('contactForm');
+    window.alert("DOM completamente cargado y parseado");
+    console.log(eventLoaded);
+    console.log(myform);  // Log el formulario para inspección
+    debugger;
+};
+window.addEventListener('DOMContentLoaded', loaded1);
+
+const loaded2 = (eventLoaded) => {
+    let myform = document.getElementById('contactForm');
+    window.alert("DOM completamente");
+    console.log(eventLoaded);
+
+    myform.addEventListener('submit', (eventSubmit) => {
+        console.log(eventSubmit);
+        debugger;
+    });
+};
+
+window.addEventListener('DOMContentLoaded', loaded2);
